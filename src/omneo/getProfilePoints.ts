574294a509bf0d1c334981d704dcb8726e827b78
oliverarkade <oliver@arkade.com.au>
2022-12-21 16:23:27 +1100

    feat: add profile points

--- src/omneo/getProfilePoints.ts
+++ src/omneo/getProfilePoints.ts
@@ -1,14 +1,14 @@
 import { Omneo } from '..'
 import { Reward } from '../types'
 
-async function getProfileRewards (this: Omneo, profileID: string, params: object): Promise<Reward> {
+async function getProfilePoints (this: Omneo, profileID: string, params: object): Promise<Reward> {
   return this.call({
     method: 'get',
-    endpoint: `/profiles/${profileID}/rewards`,
+    endpoint: `/profiles/${profileID}/points`,
     params
   }).then((response) => {
     return response.data
   })
 }
 
-export default getProfileRewards
+export default getProfilePoints
