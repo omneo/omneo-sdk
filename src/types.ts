a3a940cdc4eef2515150941dd61104b2038a1adb
oliverarkade <oliver@arkade.com.au>
2023-03-06 10:16:48 +1100

    fix: remove commas in types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
 export type OmneoConfig = {}
 
 export type OmneoClassOptions = {
-  config?: OmneoConfig,
-  tenant: string,
+  config?: OmneoConfig
+  tenant: string
   token: string
 }
 
@@ -12,9 +12,9 @@ export type Identity = {
   profile_id: string
   is_primary: boolean
   is_active: boolean
-  identifier: string,
-  handle: string,
-  created_at: Date,
+  identifier: string
+  handle: string
+  created_at: Date
   updated_at: Date
 }
 
@@ -26,176 +26,176 @@ export type IdentityRequest = {
 }
 
 export type Address = {
-  address_line_1: string,
-  address_line_2: string,
-  address_line_3: string,
-  company: string,
-  is_default: Boolean,
-  latitude: string,
-  longitude: string,
-  city: string,
-  state: string,
-  postcode: string,
-  country: string,
-  iso: string,
-  iso_state: string,
+  address_line_1: string
+  address_line_2: string
+  address_line_3: string
+  company: string
+  is_default: Boolean
+  latitude: string
+  longitude: string
+  city: string
+  state: string
+  postcode: string
+  country: string
+  iso: string
+  iso_state: string
   notes: string
 }
 
 export type AddressRequest = Partial<Address> & {
-  address_line_2?: string,
-  address_line_3?: string,
-  company?: string,
-  is_default?: Boolean,
-  latitude?: string,
+  address_line_2?: string
+  address_line_3?: string
+  company?: string
+  is_default?: Boolean
+  latitude?: string
   longitude?: string
 }
 
 export type AddressUpdateRequest = {
-  address_line_1?: string,
-  address_line_2?: string,
-  address_line_3?: string,
-  company?: string,
-  is_default?: Boolean,
-  latitude?: string,
-  longitude?: string,
-  city?: string,
-  state?: string,
-  postcode?: string,
-  country?: string,
-  iso?: string,
-  iso_state?: string,
+  address_line_1?: string
+  address_line_2?: string
+  address_line_3?: string
+  company?: string
+  is_default?: Boolean
+  latitude?: string
+  longitude?: string
+  city?: string
+  state?: string
+  postcode?: string
+  country?: string
+  iso?: string
+  iso_state?: string
   notes?: string
 }
 
 export type ProfileComms = {
-  profile_id: string,
-  email_optout: Boolean | null,
-  push_optout: Boolean | null,
-  sms_optout: Boolean | null,
-  phone_optout: Boolean | null,
-  post_optout: Boolean | null,
-  email_bounced: Boolean | null,
-  push_bounced: Boolean | null,
-  sms_bounced: Boolean | null,
-  phone_bounced: Boolean | null,
-  post_bounced: Boolean | null,
-  sms_promo: Boolean | null,
-  push_promo: Boolean | null,
-  email_promo: Boolean | null,
-  phone_promo: Boolean | null,
-  post_promo: Boolean | null,
-  email_discover: Boolean | null,
-  email_benefits: Boolean | null,
-  email_reminders: Boolean | null,
-  email_account: Boolean | null,
-  email_bookings: Boolean | null,
-  email_feedback: Boolean | null,
-  email_location: Boolean | null,
-  email_service: Boolean | null,
-  sms_discover: Boolean | null,
-  sms_benefits: Boolean | null,
-  sms_reminders: Boolean | null,
-  sms_account: Boolean | null,
-  sms_bookings: Boolean | null,
-  sms_feedback: Boolean | null,
-  sms_location: Boolean | null,
-  sms_service: Boolean | null,
-  push_discover: Boolean | null,
-  push_benefits: Boolean | null,
-  push_reminders: Boolean | null,
-  push_account: Boolean | null,
-  push_bookings: Boolean | null,
-  push_feedback: Boolean | null,
-  push_location: Boolean | null,
-  push_service: Boolean | null,
-  phone_discover: Boolean | null,
-  phone_benefits: Boolean | null,
-  phone_reminders: Boolean | null,
-  phone_account: Boolean | null,
-  phone_bookings: Boolean | null,
-  phone_feedback: Boolean | null,
-  phone_location: Boolean | null,
-  phone_service: Boolean | null,
-  post_discover: Boolean | null,
-  post_benefits: Boolean | null,
-  post_reminders: Boolean | null,
-  post_account: Boolean | null,
-  post_bookings: Boolean | null,
-  post_feedback: Boolean | null,
-  post_location: Boolean | null,
-  post_service: Boolean | null,
-  email_verification: 'sent' | 'not_sent',
-  sms_verification: 'sent' | 'not_sent',
-  phone_verification: 'sent' | 'not_sent',
-  post_verification: 'sent' | 'not_sent',
-  terms_accepted_at: Boolean | null | null,
-  terms_accepted_version: Boolean | null | null,
-  created_at: Date,
+  profile_id: string
+  email_optout: Boolean | null
+  push_optout: Boolean | null
+  sms_optout: Boolean | null
+  phone_optout: Boolean | null
+  post_optout: Boolean | null
+  email_bounced: Boolean | null
+  push_bounced: Boolean | null
+  sms_bounced: Boolean | null
+  phone_bounced: Boolean | null
+  post_bounced: Boolean | null
+  sms_promo: Boolean | null
+  push_promo: Boolean | null
+  email_promo: Boolean | null
+  phone_promo: Boolean | null
+  post_promo: Boolean | null
+  email_discover: Boolean | null
+  email_benefits: Boolean | null
+  email_reminders: Boolean | null
+  email_account: Boolean | null
+  email_bookings: Boolean | null
+  email_feedback: Boolean | null
+  email_location: Boolean | null
+  email_service: Boolean | null
+  sms_discover: Boolean | null
+  sms_benefits: Boolean | null
+  sms_reminders: Boolean | null
+  sms_account: Boolean | null
+  sms_bookings: Boolean | null
+  sms_feedback: Boolean | null
+  sms_location: Boolean | null
+  sms_service: Boolean | null
+  push_discover: Boolean | null
+  push_benefits: Boolean | null
+  push_reminders: Boolean | null
+  push_account: Boolean | null
+  push_bookings: Boolean | null
+  push_feedback: Boolean | null
+  push_location: Boolean | null
+  push_service: Boolean | null
+  phone_discover: Boolean | null
+  phone_benefits: Boolean | null
+  phone_reminders: Boolean | null
+  phone_account: Boolean | null
+  phone_bookings: Boolean | null
+  phone_feedback: Boolean | null
+  phone_location: Boolean | null
+  phone_service: Boolean | null
+  post_discover: Boolean | null
+  post_benefits: Boolean | null
+  post_reminders: Boolean | null
+  post_account: Boolean | null
+  post_bookings: Boolean | null
+  post_feedback: Boolean | null
+  post_location: Boolean | null
+  post_service: Boolean | null
+  email_verification: 'sent' | 'not_sent'
+  sms_verification: 'sent' | 'not_sent'
+  phone_verification: 'sent' | 'not_sent'
+  post_verification: 'sent' | 'not_sent'
+  terms_accepted_at: Boolean | null | null
+  terms_accepted_version: Boolean | null | null
+  created_at: Date
   updated_at: Date
 }
 
 export type ProfileAppearance = {
-  profile_id: string,
-  hair_colour: string | null,
-  hair_length: string | null,
-  shape_body: string | null,
-  shape_face: string | null,
-  size_cup: string | null,
-  size_hat: string | null,
-  size_formal_jacket: string | null,
-  size_formal_jacket_length: string | null,
-  size_formal_shirt: string | null,
-  size_formal_shirt_fit: string | null,
-  size_formal_trouser: string | null,
-  size_formal_trouser_drop: string | null,
-  size_formal_trouser_leg: string | null,
-  size_jacket: string | null,
-  size_pant: string | null,
-  size_shoe: string | null,
-  size_type_age: string | null,
-  size_type_region: string | null,
-  size_top: string | null,
-  size_gloves: string | null,
-  size_swimwear_top: string | null,
-  size_swimwear_bottom: string | null,
-  size_swimwear_cup: string | null,
-  size_dress: string | null,
-  skin_type: string | null,
-  measurement_arm: string | null,
-  measurement_arm_inside: string | null,
-  measurement_bust: string | null,
-  measurement_chest: string | null,
-  measurement_foot_length: string | null,
-  measurement_head: string | null,
-  measurement_height: string | null,
-  measurement_hip: string | null,
-  measurement_leg_inside: string | null,
-  measurement_leg_outside: string | null,
-  measurement_neck: string | null,
-  measurement_neck_to_wrist: string | null,
-  measurement_shoulders: string | null,
-  measurement_waist: string | null,
-  measurement_weight: string | null,
-  brow_colour: string | null,
-  eye_colour: string | null,
-  measurement_hand_right_thumb: string | null,
-  measurement_hand_right_index: string | null,
-  measurement_hand_right_middle: string | null,
-  measurement_hand_right_ring: string | null,
-  measurement_hand_right_pinky: string | null,
-  measurement_hand_left_thumb: string | null,
-  measurement_hand_left_index: string | null,
-  measurement_hand_left_middle: string | null,
-  measurement_hand_left_ring: string | null,
-  measurement_hand_left_pinky: string | null,
-  measurement_ear_helix: string | null,
-  measurement_ear_forward_helix: string | null,
-  measurement_ear_flat: string | null,
-  measurement_ear_conch: string | null,
-  measurement_ear_tragus: string | null,
-  measurement_ear_lobe: string | null,
-  created_at: Date,
+  profile_id: string
+  hair_colour: string | null
+  hair_length: string | null
+  shape_body: string | null
+  shape_face: string | null
+  size_cup: string | null
+  size_hat: string | null
+  size_formal_jacket: string | null
+  size_formal_jacket_length: string | null
+  size_formal_shirt: string | null
+  size_formal_shirt_fit: string | null
+  size_formal_trouser: string | null
+  size_formal_trouser_drop: string | null
+  size_formal_trouser_leg: string | null
+  size_jacket: string | null
+  size_pant: string | null
+  size_shoe: string | null
+  size_type_age: string | null
+  size_type_region: string | null
+  size_top: string | null
+  size_gloves: string | null
+  size_swimwear_top: string | null
+  size_swimwear_bottom: string | null
+  size_swimwear_cup: string | null
+  size_dress: string | null
+  skin_type: string | null
+  measurement_arm: string | null
+  measurement_arm_inside: string | null
+  measurement_bust: string | null
+  measurement_chest: string | null
+  measurement_foot_length: string | null
+  measurement_head: string | null
+  measurement_height: string | null
+  measurement_hip: string | null
+  measurement_leg_inside: string | null
+  measurement_leg_outside: string | null
+  measurement_neck: string | null
+  measurement_neck_to_wrist: string | null
+  measurement_shoulders: string | null
+  measurement_waist: string | null
+  measurement_weight: string | null
+  brow_colour: string | null
+  eye_colour: string | null
+  measurement_hand_right_thumb: string | null
+  measurement_hand_right_index: string | null
+  measurement_hand_right_middle: string | null
+  measurement_hand_right_ring: string | null
+  measurement_hand_right_pinky: string | null
+  measurement_hand_left_thumb: string | null
+  measurement_hand_left_index: string | null
+  measurement_hand_left_middle: string | null
+  measurement_hand_left_ring: string | null
+  measurement_hand_left_pinky: string | null
+  measurement_ear_helix: string | null
+  measurement_ear_forward_helix: string | null
+  measurement_ear_flat: string | null
+  measurement_ear_conch: string | null
+  measurement_ear_tragus: string | null
+  measurement_ear_lobe: string | null
+  created_at: Date
   updated_at: Date
 }
 
@@ -308,8 +308,8 @@ export type Profile = {
   custom_attributes: Array<CustomAttribute>
   address: Address
   addresses: Array<Address>
-  reward_balance: number,
-  point_balance: number,
+  reward_balance: number
+  point_balance: number
   combined_balance_dollars: number
   point_balance_dollars: number
   profile_type: 'pending' | 'temporary' | 'active' | 'deleted'
@@ -331,9 +331,9 @@ export type RequestParams = {[key: string]: any}
 export type RequestBody = {[key: string]: any}
 
 export type OmneoRequest = {
-  method: string,
-  endpoint: string,
-  params?: RequestParams,
+  method: string
+  endpoint: string
+  params?: RequestParams
   body?: RequestBody
 }
 
@@ -444,47 +444,47 @@ export type Reward = {
 }
 
 export type PointDefinition = {
-  id: number,
-  name: string,
-  handle: string,
-  description: string | null,
-  notes: string | null,
-  is_reassignable: boolean,
-  tags: Array<string>,
-  created_at: Date,
+  id: number
+  name: string
+  handle: string
+  description: string | null
+  notes: string | null
+  is_reassignable: boolean
+  tags: Array<string>
+  created_at: Date
   updated_at: Date
 }
 
 export type Point = {
-  id: number,
-  profile_id: string,
-  linked_profile_id: string | null,
-  value_initial: number,
-  value_remaining: number,
-  issued_at: Date,
-  expires_at: Date,
-  definition: PointDefinition,
-  created_at: Date,
-  updated_at: Date,
-  source_type: string,
-  source: object, // Add Point sources type transaction_item
-  rate: number | null,
-  description: string | null,
+  id: number
+  profile_id: string
+  linked_profile_id: string | null
+  value_initial: number
+  value_remaining: number
+  issued_at: Date
+  expires_at: Date
+  definition: PointDefinition
+  created_at: Date
+  updated_at: Date
+  source_type: string
+  source: object // Add Point sources type transaction_item
+  rate: number | null
+  description: string | null
   meta: object | null
 }
 
 export type TenantCustomFieldTypes = 'string' | 'integer' | 'float' | 'boolean' | 'json' | 'array'
 
 export type TenantCustomFieldRequest = {
-  name: string,
-  handle: string,
-  namespace: string,
-  value: any,
+  name: string
+  handle: string
+  namespace: string
+  value: any
   type: TenantCustomFieldTypes
 }
 
 export type APITokenRequest = {
-  name: string,
+  name: string
   scopes: Array<string>
 }
 
@@ -497,7 +497,7 @@ export type TransactionItem = {
   product_variant_id: number | null
   sku: string | null
   variant_external_id: string | null
-  is_void: boolean,
+  is_void: boolean
   quantity: number
   price_current: number
   price_sell: number
@@ -517,7 +517,7 @@ export type TransactionItem = {
 }
 
 export type Transaction = {
-  id: string,
+  id: string
   external_id: string
   profile_id?: string
   profile?: {
