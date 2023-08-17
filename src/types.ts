bf2c186d9353f34ae7fa7fb4a1ee089638fe2cad
oliverarkade <oliver@arkade.com.au>
2023-08-17 18:23:46 +1000

    fix: Correct AddressRequest partial typings

--- src/types.ts
+++ src/types.ts
@@ -40,15 +40,15 @@ export type Address = {
   iso: string
   iso_state: string
   notes: string
+  meta?: {[key: string]: any}
 }
 
 export type AddressRequest = Partial<Address> & {
-  address_line_2?: string
-  address_line_3?: string
-  company?: string
-  is_default?: Boolean
-  latitude?: string
-  longitude?: string
+  address_line_1: string
+  city: string,
+  postcode: string,
+  country: string,
+  state: string
 }
 
 export type AddressUpdateRequest = {
