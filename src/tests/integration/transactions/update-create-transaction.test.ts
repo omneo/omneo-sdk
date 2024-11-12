c617df7079ebae405e37b5011c6c65da8ea6862e
kyle-arkade <kyle@arkade.com.au>
2024-11-12 15:25:16 +1100

    feat: added test for TXN with product_variant_id

--- src/tests/integration/transactions/update-create-transaction.test.ts
+++ src/tests/integration/transactions/update-create-transaction.test.ts
@@ -1,35 +1,47 @@
 import { afterEach, expect, test } from 'vitest'
-import { writeTransaction } from '../../mocks/transactions/transaction'
+import {writeTransactionWithVariant, writeTransactionWithVariantID} from '../../mocks/transactions/transaction'
 import simpleOmneoRequest from '../../lib/simple-omneo-request'
 import { Omneo } from '../../../omneo'
 import randomString from '../../lib/string/random'
 
-let NEW_TRANSACTION_ID : number | null = null
-const EXTERNAL_ID = randomString(9)
+const CREATED_TRANSACTION_IDS : number[] = []
 
 const omneo = new Omneo({
   tenant: process.env.OMNEO_TENANT as string,
   token: process.env.OMNEO_TOKEN as string
 })
 
-test('SDK can write a transaction to Omneo.', async () => {
-  writeTransaction.external_id = EXTERNAL_ID
+test('SDK can write a transaction with a variant to Omneo.', async () => {
+  writeTransactionWithVariant.external_id = randomString(9)
 
-  const sdkCreateTransaction = await omneo.transactions.updateCreate(writeTransaction)
+  const sdkCreateTransaction = await omneo.transactions.updateCreate(writeTransactionWithVariant)
 
-  expect(sdkCreateTransaction.external_id).toBe(EXTERNAL_ID)
-  expect(sdkCreateTransaction.tags).toStrictEqual(writeTransaction.tags)
-  expect(sdkCreateTransaction.receipt_ref).toStrictEqual(writeTransaction.receipt_ref)
-  NEW_TRANSACTION_ID = sdkCreateTransaction.id
+  expect(sdkCreateTransaction.external_id).toBe(writeTransactionWithVariant.external_id)
+  expect(sdkCreateTransaction.tags).toStrictEqual(writeTransactionWithVariant.tags)
+  expect(sdkCreateTransaction.receipt_ref).toStrictEqual(writeTransactionWithVariant.receipt_ref)
+  CREATED_TRANSACTION_IDS.push(sdkCreateTransaction.id)
+})
+
+test('SDK can write a transaction with a variant ID Omneo.', async () => {
+  writeTransactionWithVariantID.external_id = randomString(9)
+
+  const sdkCreateTransaction = await omneo.transactions.updateCreate(writeTransactionWithVariantID)
+
+  expect(sdkCreateTransaction.external_id).toBe(writeTransactionWithVariantID.external_id)
+  expect(sdkCreateTransaction.tags).toStrictEqual(writeTransactionWithVariantID.tags)
+  expect(sdkCreateTransaction.receipt_ref).toStrictEqual(writeTransactionWithVariantID.receipt_ref)
+  CREATED_TRANSACTION_IDS.push(sdkCreateTransaction.id)
 })
 
 afterEach(async () => {
   // Delete the test transaction.
-  if (NEW_TRANSACTION_ID) {
-    console.log('Cleaning up transaction', NEW_TRANSACTION_ID)
-    const deleteResponse = await simpleOmneoRequest('DELETE', `/transactions/${NEW_TRANSACTION_ID}`)
-    if (deleteResponse.status === 204) {
-      console.log(`Transaction ${NEW_TRANSACTION_ID} deleted`)
+  if (CREATED_TRANSACTION_IDS.length > 0) {
+    for (const transactionId of CREATED_TRANSACTION_IDS) {
+      console.log('Cleaning up transaction', transactionId)
+      const deleteResponse = await simpleOmneoRequest('DELETE', `/transactions/${transactionId}`)
+      if (deleteResponse.status === 204) {
+        console.log(`Transaction ${transactionId} deleted`)
+      }
     }
   }
 })
--- src/tests/mocks/transactions/transaction.ts
+++ src/tests/mocks/transactions/transaction.ts
@@ -1,10 +1,10 @@
 import { Transaction, TransactionInput } from '../../../types'
 
-export const writeTransaction: TransactionInput = {
-  profile_id: '9d7796e5-f8a0-4611-aa61-e6872a6a4393',
+export const writeTransactionWithVariant: TransactionInput = {
+  profile_id: process.env.OMNEO_TEST_PROFILE_ID,
   external_id: '9999999',
   receipt_ref: 'D172009999999',
-  location_id: '49',
+  location_id: process.env.OMNEO_TEST_LOCATION_ID,
   total: 100,
   total_original: 100,
   systems: [
@@ -22,7 +22,7 @@ export const writeTransaction: TransactionInput = {
     price_tax: 10,
     price_original: 100,
     product_variant: {
-      product_id: 1981205,
+      product_id: parseInt(process.env.OMNEO_TEST_PRODUCT_ID),
       sku: 'gh-test-action-item-1',
       barcode: 'GHT-0001',
       title: 'GH ACTION TEST ITEM 1',
@@ -40,16 +40,48 @@ export const writeTransaction: TransactionInput = {
   }
 }
 
+export const writeTransactionWithVariantID: TransactionInput = {
+  profile_id: process.env.OMNEO_TEST_PROFILE_ID,
+  external_id: '9999999',
+  receipt_ref: 'D1729859045',
+  location_id: process.env.OMNEO_TEST_LOCATION_ID,
+  total: 100,
+  total_original: 100,
+  systems: [
+    'CI'
+  ],
+  tags: [
+    'ci',
+    'test'
+  ],
+  items: [{
+    name: 'GH ACTION TEST ITEM',
+    quantity: 1,
+    price_current: 100,
+    price_sell: 100,
+    price_tax: 10,
+    price_original: 100,
+    product_variant_id: parseInt(process.env.OMNEO_TEST_PRODUCT_VARIANT_ID),
+    discounts: []
+  }],
+  payments: [],
+  timezone: 'Australia/Melbourne',
+  transacted_at: '2024-11-11 12:00:00',
+  meta: {
+    test: 'test'
+  }
+}
+
 export const readTransaction: Transaction = {
   id: 9999999,
   external_id: '9999999',
   redemption: null,
-  profile_id: '9a690c37-5bec-44f4-b6b3-8cbe11e46276',
+  profile_id: process.env.OMNEO_TEST_PROFILE_ID,
   profile: {
     email: 'subscriptions+githubactions@arkade.com.au'
   },
   location: {
-    id: 49,
+    id: 1,
     type: null,
     name: null,
     description: null,
@@ -63,23 +95,23 @@ export const readTransaction: Transaction = {
   meta: {
     web_order_number: null
   },
-  total: 69,
-  total_original: 69,
-  total_converted: 69,
+  total: 100,
+  total_original: 100,
+  total_converted: 100,
   systems: [
     'CI'
   ],
   rounding: null,
-  margin: 43,
+  margin: 50,
   is_void: false,
   tags: [],
   items: [{
-    id: 2542,
+    id: 1,
     external_id: null,
     name: 'GH ACTION TEST ITEM',
-    transaction_id: 7479194,
-    product_id: 227266,
-    product_variant_id: 227266,
+    transaction_id: 111111111,
+    product_id: 1,
+    product_variant_id: 1,
     sku: null,
     variant_external_id: null,
     is_void: false,
@@ -98,10 +130,10 @@ export const readTransaction: Transaction = {
     updated_at: '2024-02-13 04:07:46',
     pivot: [],
     product_variant: {
-      id: 227266,
-      product_id: 227266,
+      id: 1,
+      product_id: 1,
       sku: '1299388',
-      external_id: 227266,
+      external_id: 1111,
       barcode: '',
       web_url: null,
       handle: null,
@@ -157,12 +189,12 @@ export const readTransaction: Transaction = {
     full_name: null,
     email: 'staff-profile-sqdzljthztvdvsjn@omneo.io',
     identities: [{
-      id: 1955,
+      id: 1,
       handle: 'staff',
       identifier: '73648',
       is_primary: false,
       is_active: true,
-      profile_id: '9b461544-f0db-44b6-b559-0f698c881229',
+      profile_id: '9b461544-f0db-44b6-b559-0f698c881555',
       merged_from: null,
       created_at: '2024-02-06 20:57:48',
       updated_at: '2024-02-06 20:57:48'
