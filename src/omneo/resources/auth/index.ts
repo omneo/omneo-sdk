bb9c4af76e5897037b0af0cc4c197548d2d35773
Oliver Tribe <oliver@arkade.com.au>
2024-11-25 12:29:17 +1100

    feat: add API token Get, Get ID, Delete

--- src/omneo/resources/auth/index.ts
+++ src/omneo/resources/auth/index.ts
@@ -1,14 +1,41 @@
-import { APITokenRequest } from '../../../types'
+import { APIToken, APITokenInput } from '../../../types'
 import Resource from '../resource.js'
 
 export default class Auth extends Resource {
-  createAPIToken (body: APITokenRequest): Promise<any> {
+  createAPIToken (body: APITokenInput): Promise<Omit<APIToken, 'client' | 'user'>> {
     return this.client.call({
       method: 'post',
       endpoint: '/auth/api-tokens',
       body
     }).then((response) => {
+      const { client, user, ...token } = response
+      return token
+    })
+  }
+
+  getAPITokens (params: object): Promise<APIToken[]> {
+    return this.client.call({
+      method: 'post',
+      endpoint: '/auth/api-tokens',
+      params
+    }).then((response) => {
       return response
+    })
+  }
+
+  getAPITokenByID (id: string): Promise<APIToken> {
+    return this.client.call({
+      method: 'get',
+      endpoint: `/auth/api-tokens/${id}`
+    }).then((response) => {
+      return response
+    })
+  }
+
+  deleteAPIToken (id: string): Promise<void> {
+    return this.client.call({
+      method: 'delete',
+      endpoint: `/auth/api-tokens/${id}`
     })
   }
 }
