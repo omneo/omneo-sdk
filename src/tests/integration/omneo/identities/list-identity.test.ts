687ca6b542079f9a50f8d7935b1d97ed88e7390a
Johny Xu <johny.xu@arkade.com.au>
2024-11-29 10:35:50 +1100

    test: update identity unit test

--- src/tests/integration/omneo/identities/list-identity.test.ts
+++ src/tests/integration/omneo/identities/list-identity.test.ts
@@ -1,7 +1,14 @@
-import { describe, test, expect } from 'vitest'
-
+import { describe, expect, test, afterAll } from 'vitest'
+import jwt from 'jsonwebtoken'
 import { IdentityResponse, RequestParams } from '../../../../types'
 import { Omneo } from '../../../../omneo'
+import simpleOmneoRequest from '../../../lib/simple-omneo-request'
+import { testWithIDData } from '../../id/test-with-id-data'
+import randomString from '../../../lib/string/random'
+
+const CREATED_IDENTITIES : string[] = []
+const identityHandleName = 'sdk_unit_test_identity'
+const PROFILE_IDS : string[] = []
 
 const omneo = new Omneo({
   tenant: process.env.OMNEO_TENANT as string,
@@ -11,59 +18,81 @@ const omneo = new Omneo({
 describe('Identities list', () => {
   test('SDK can get identities.', async () => {
     const sdkIdentityList = await omneo.identities.list()
-    sdkIdentityList.forEach((identity) => {
-      expect(identity).toHaveProperty('id')
-      expect(identity).toHaveProperty('handle')
-      expect(identity).toHaveProperty('identifier')
-      expect(identity).toHaveProperty('profile_id')
-      expect(identity).toHaveProperty('is_active')
-    })
+    expect(Array.isArray(sdkIdentityList)).toBe(true)
   })
-  test('SDK can get identities.', async () => {
+  testWithIDData('SDK can get identities with filters', async ({ IDData }) => {
+    const { tokenData } = IDData
+    const { pid: decodedProfileID } = jwt.decode(tokenData.token)
+    PROFILE_IDS.push(decodedProfileID)
+    const payload = {
+      handle: identityHandleName,
+      identifier: randomString(11)
+    }
+    const { id, handle, profile_id: profileID } = await omneo.profiles.createIdentity(decodedProfileID, payload)
+    expect(profileID).toBe(decodedProfileID)
+    expect(handle).toBe(identityHandleName)
+    CREATED_IDENTITIES.push(id)
+
     const params: RequestParams = {
-      'filter[handle]': 'apple_device_id',
+      'filter[handle]': identityHandleName,
       include: 'profile'
     }
-    const sdkIdentityList = await omneo.identities.list(params)
-    sdkIdentityList.forEach((identity) => {
-      expect(identity).toHaveProperty('id')
-      expect(identity).toHaveProperty('handle', 'apple_device_id')
-      expect(identity).toHaveProperty('identifier')
-      expect(identity).toHaveProperty('profile_id')
-      expect(identity).toHaveProperty('is_active')
-      expect(identity).toHaveProperty('profile')
-    })
+    const sdkIdentity = await omneo.identities.list(params).then((data) => data[0])
+    expect(sdkIdentity.id).toBeTypeOf('number')
+    expect(sdkIdentity.profile_id).toBeTypeOf('string')
+    expect(sdkIdentity.identifier).toBeTypeOf('string')
+    expect(sdkIdentity).toEqual(expect.objectContaining({
+      handle: identityHandleName,
+      is_active: true
+    }))
   })
-  test('SDK can get identities.', async () => {
+  testWithIDData('SDK can get identities with pagination', async ({ IDData }) => {
+    const { tokenData } = IDData
+    const { pid: decodedProfileID } = jwt.decode(tokenData.token)
+    PROFILE_IDS.push(decodedProfileID)
+    const payload = {
+      handle: identityHandleName,
+      identifier: randomString(11)
+    }
+    const { id, handle, profile_id: profileID } = await omneo.profiles.createIdentity(decodedProfileID, payload)
+    expect(profileID).toBe(decodedProfileID)
+    expect(handle).toBe(identityHandleName)
+    CREATED_IDENTITIES.push(id)
+
     const params: RequestParams = {
       'filter[handle]': 'apple_device_id',
       include: 'profile',
       withPagination: true
     }
-    const sdkIdentityList: IdentityResponse = await omneo.identities.list(params)
-    sdkIdentityList.data.forEach((identity) => {
-      expect(identity).toHaveProperty('id')
-      expect(identity).toHaveProperty('handle', 'apple_device_id')
-      expect(identity).toHaveProperty('identifier')
-      expect(identity).toHaveProperty('profile_id')
-      expect(identity).toHaveProperty('is_active')
-      expect(identity).toHaveProperty('profile')
-    })
+    const { links, meta }: IdentityResponse = await omneo.identities.list(params)
 
     // links
-    expect(sdkIdentityList.links).toHaveProperty('first')
-    expect(sdkIdentityList.links).toHaveProperty('last')
-    expect(sdkIdentityList.links).toHaveProperty('prev')
-    expect(sdkIdentityList.links).toHaveProperty('next')
+    expect(links.first).toBeTypeOf('string')
+    expect(links.last).toBeTypeOf('string')
+    expect(links).toHaveProperty('prev')
+    expect(links).toHaveProperty('next')
 
     // meta
-    expect(sdkIdentityList.meta).toHaveProperty('current_page')
-    expect(sdkIdentityList.meta).toHaveProperty('from')
-    expect(sdkIdentityList.meta).toHaveProperty('last_page')
-    expect(sdkIdentityList.meta).toHaveProperty('links')
-    expect(sdkIdentityList.meta).toHaveProperty('path')
-    expect(sdkIdentityList.meta).toHaveProperty('per_page')
-    expect(sdkIdentityList.meta).toHaveProperty('to')
-    expect(sdkIdentityList.meta).toHaveProperty('total')
+    expect(meta.current_page).toBeTypeOf('number')
+    expect(meta.from).toBeTypeOf('number')
+    expect(meta.last_page).toBeTypeOf('number')
+    expect(meta).toHaveProperty('links')
+    expect(meta.path).toBeTypeOf('string')
+    expect(meta.per_page).toBeTypeOf('number')
+    expect(meta.to).toBeTypeOf('number')
+    expect(meta.total).toBeTypeOf('number')
   })
+})
+
+afterAll(async () => {
+  if (PROFILE_IDS.length > 0 && CREATED_IDENTITIES.length > 0) {
+    const profileId: string = PROFILE_IDS[0]
+    for (const identityId of CREATED_IDENTITIES) {
+      console.log('Cleaning up SDK Identity with ID', identityId)
+      const deleteResponse = await simpleOmneoRequest('DELETE', `/profiles/${profileId}/identities/${identityId}`)
+      if (deleteResponse.status === 204) {
+        console.log(`SDK Identity ID ${identityId} deleted`)
+      }
+    }
+  }
 })
