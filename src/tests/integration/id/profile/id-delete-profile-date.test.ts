2b0a5347dd8ae9c3a49c1dac9f8336bd23c7dd84
Johny Xu <johny.xu@arkade.com.au>
2024-12-04 09:57:21 +1100

    fix: unit test if the deletion fails

--- src/tests/integration/id/profile/id-delete-profile-date.test.ts
+++ src/tests/integration/id/profile/id-delete-profile-date.test.ts
@@ -1,10 +1,11 @@
-import { describe, expect } from 'vitest'
+import { describe, expect, afterAll } from 'vitest'
 import { ID } from '../../../../id'
 import { testWithIDData } from '../test-with-id-data'
 import { ProfileDatesAttribute } from '../../../../types'
 import simpleOmneoRequest from '../../../lib/simple-omneo-request'
 import randomString from '../../../lib/string/random'
 
+const FAILED_DELETE_DATES : string[] = []
 const getHandle = () => { return `sdk_unit_test_dates_${randomString(5).toLowerCase()}_${Math.floor(Date.now() / 1000)}` }
 const testProfileID = process.env.OMNEO_TEST_PROFILE_ID as string
 
@@ -38,8 +39,23 @@ describe('ID Delete Date', () => {
     const filterDates = dates.filter(d => d.handle === payload.handle)
     expect(filterDates.length).toBeGreaterThan(0)
 
-    const deleteDates: ProfileDatesAttribute[] = await IDClient.profile.deleteDate(deleteDateId)
+    const deleteDates: ProfileDatesAttribute[] = await IDClient.profile.deleteDate(deleteDateId).catch(() => {
+      FAILED_DELETE_DATES.push(deleteDateId)
+      throw new Error('ID Failed to delete Profile Date')
+    })
     const filterDeleteDates = deleteDates.filter(d => d.handle === payload.handle)
     expect(filterDeleteDates.length).toBe(0)
   })
+})
+
+afterAll(async () => {
+  if (FAILED_DELETE_DATES.length > 0) {
+    for (const handle of FAILED_DELETE_DATES) {
+      console.log('ID Cleaning up Profile Date with ID', handle)
+      const deleteResponse = await simpleOmneoRequest('DELETE', `profiles/${testProfileID}/attributes/dates/${handle}`)
+      if (deleteResponse.status === 204) {
+        console.log(`ID Profile Date ID ${handle} deleted`)
+      }
+    }
+  }
 })
--- src/tests/integration/omneo/profile/delete-profile-date.test.ts
+++ src/tests/integration/omneo/profile/delete-profile-date.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, afterAll } from 'vitest'
 import { Omneo } from '../../../../omneo'
 import { ProfileDatesAttribute } from '../../../../types'
 import simpleOmneoRequest from '../../../lib/simple-omneo-request'
@@ -9,6 +9,7 @@ const omneo = new Omneo({
   token: process.env.OMNEO_TOKEN as string
 })
 
+const FAILED_DELETE_DATES : string[] = []
 const getHandle = () => { return `sdk_unit_test_dates_${randomString(5).toLowerCase()}_${Math.floor(Date.now() / 1000)}` }
 const testProfileID = process.env.OMNEO_TEST_PROFILE_ID as string
 
@@ -36,8 +37,23 @@ describe('Profile Date Delete', () => {
     const filterDates = dates.filter(d => d.handle === payload.handle)
     expect(filterDates.length).toBeGreaterThan(0)
 
-    const deleteDates: ProfileDatesAttribute[] = await omneo.profiles.deleteProfileDate(testProfileID, deleteDateId)
+    const deleteDates: ProfileDatesAttribute[] = await omneo.profiles.deleteProfileDate(testProfileID, deleteDateId).catch(() => {
+      FAILED_DELETE_DATES.push(deleteDateId)
+      throw new Error('Omneo Failed to delete Profile Date')
+    })
     const filterDeleteDates = deleteDates.filter(d => d.handle === payload.handle)
     expect(filterDeleteDates.length).toBe(0)
   })
+})
+
+afterAll(async () => {
+  if (FAILED_DELETE_DATES.length > 0) {
+    for (const handle of FAILED_DELETE_DATES) {
+      console.log('Omneo Cleaning up Profile Date with ID', handle)
+      const deleteResponse = await simpleOmneoRequest('DELETE', `profiles/${testProfileID}/attributes/dates/${handle}`)
+      if (deleteResponse.status === 204) {
+        console.log(`Omneo Profile Date ID ${handle} deleted`)
+      }
+    }
+  }
 })
