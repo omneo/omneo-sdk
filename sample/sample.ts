a31ee550d9fe2798bc5859011dbfc03d368fcced
Johny Xu <johny.xu@arkade.com.au>
2024-12-18 16:56:50 +1100

    fix: update sample

--- sample/sample.ts
+++ sample/sample.ts
@@ -1,23 +1,13 @@
-
 import * as dotenv from 'dotenv'
 import { Omneo } from '../src'
-// import { Omneo, ID } from '../src'
-// import simpleOmneoRequest from '../src/test/lib/simple-omneo-request'
 
 dotenv.config()
 
-const { OMNEO_TOKEN: token, OMNEO_TENANT: tenant, OMNEO_TEST_PROFILE_ID: testProfileId } = process.env
+const { OMNEO_TOKEN: token, OMNEO_TENANT: tenant } = process.env
 
-const profileId = testProfileId as string
+const omneoClient = new Omneo({ token: token as string, tenant: tenant as string })
 
-function main () {
-  const omneoClient = new Omneo({ token: token as string, tenant: tenant as string })
-
-  console.log('profileId: ', profileId)
-  // const IDClient = new ID({ tenant: tenant as string, omneoAPIToken: token, config: {} })
-  // IDClient.auth.requestAuthToken({ id: testProfileId as string }).then((res) => {
-  //   console.log('response:', res)
-  // })
-}
-
-main()
+console.log('Fetching Profiles')
+omneoClient.profiles.list().then((profiles) => {
+  console.log(`Retrieved ${profiles.length} profiles`)
+})
