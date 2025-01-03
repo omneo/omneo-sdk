0c33440b05e52f6424d6d043ffb5a189620aa9f6
Oliver Tribe <oliver@arkade.com.au>
2025-01-03 16:06:36 +1100

    test: remove tier tests temporarily until omneo API updated

--- src/tests/integration/omneo/profiles/tiers/assign-tier.test.ts
+++ src/tests/integration/omneo/profiles/tiers/assign-tier.test.ts
@@ -1,35 +1,36 @@
-import { afterAll, describe, expect, test } from 'vitest'
-import { Omneo } from '../../../../..'
-import simpleOmneoRequest from '../../../../lib/simple-omneo-request'
+// Tests can be re-added once omneo bugs with calculating tiers bugs are fixed, can delete tier definitions
+import { describe } from 'vitest'
+// import { Omneo } from '../../../../..'
+// import simpleOmneoRequest from '../../../../lib/simple-omneo-request'
 
-const CREATED_TIER_DEFINITIONS = [] as number[]
+// const CREATED_TIER_DEFINITIONS = [] as number[]
 
-const omneo = new Omneo({
-  tenant: process.env.OMNEO_TENANT as string,
-  token: process.env.OMNEO_TOKEN as string
-})
+// const omneo = new Omneo({
+//   tenant: process.env.OMNEO_TENANT as string,
+//   token: process.env.OMNEO_TOKEN as string
+// })
 
-const testProfileID = process.env.OMNEO_TEST_PROFILE_ID as string
+// const testProfileID = process.env.OMNEO_TEST_PROFILE_ID as string
 
-const definitionPayload = {
-  name: `assign_profile_tier_${Date.now()}`,
-  handle: `assign_profile_tier_${Date.now()}`,
-  description: `SDK integration test - ${Date.now()}`,
-  value_min: Math.floor(Math.random() * 1000) + 1,
-  value_maintain: Math.floor(Math.random() * 1000) + 1,
-  is_assignable: true,
-  is_floor: false
-}
+// const definitionPayload = {
+//   name: `assign_profile_tier_${Date.now()}`,
+//   handle: `assign_profile_tier_${Date.now()}`,
+//   description: `SDK integration test - ${Date.now()}`,
+//   value_min: Math.floor(Math.random() * 1000) + 1,
+//   value_maintain: Math.floor(Math.random() * 1000) + 1,
+//   is_assignable: true,
+//   is_floor: false
+// }
 
 describe('Assign Profile Tier', async () => {
-  test('SDK can assign a profile tier.', async () => {
-    const definition = await simpleOmneoRequest('POST', '/tiers/definitions', definitionPayload).then(({ data }) => {
-      CREATED_TIER_DEFINITIONS.push(data.id)
-      return data
-    })
-    const tiers = await omneo.profiles.assignTier(testProfileID, definition.handle)
-    expect(tiers.current_tier?.handle === definition.handle)
-  })
+  // test('SDK can assign a profile tier.', async () => {
+  //   const definition = await simpleOmneoRequest('POST', '/tiers/definitions', definitionPayload).then(({ data }) => {
+  //     CREATED_TIER_DEFINITIONS.push(data.id)
+  //     return data
+  //   })
+  //   const tiers = await omneo.profiles.assignTier(testProfileID, definition.handle)
+  //   expect(tiers.current_tier?.handle === definition.handle)
+  // })
 })
 
 // TODO Deleting tier definitions while assigned to a profile causes API errors, re add  this code when bug fixed
--- src/tests/integration/omneo/profiles/tiers/calculate-tiers.test.ts
+++ src/tests/integration/omneo/profiles/tiers/calculate-tiers.test.ts
@@ -1,23 +1,27 @@
-import { describe, expect, test } from 'vitest'
-import { Omneo } from '../../../../..'
-import { tier as mockTier } from '../../../../mocks/tier/tier'
+// TODO Cannot calculate tiers on profiles without tier
+// Cannot delete a tier for testing
+// Tests can be added once omneo bugs are fixed
 
-const omneo = new Omneo({
-  tenant: process.env.OMNEO_TENANT as string,
-  token: process.env.OMNEO_TOKEN as string
-})
+import { describe } from 'vitest'
+// import { Omneo } from '../../../../..'
+// import { tier as mockTier } from '../../../../mocks/tier/tier'
 
-const testProfileID = process.env.OMNEO_TEST_PROFILE_ID as string
+// const omneo = new Omneo({
+//   tenant: process.env.OMNEO_TENANT as string,
+//   token: process.env.OMNEO_TOKEN as string
+// })
+
+// const testProfileID = process.env.OMNEO_TEST_PROFILE_ID as string
 
 describe('Calculate Profile Tiers', () => {
-  test('SDK can calculate a profile tier.', async () => {
-    const tiers = await omneo.profiles.calculateTiers(testProfileID)
-    Object.keys(mockTier).forEach((key) => {
-      expect(tiers).toHaveProperty(key)
-    })
+  // test('SDK can calculate a profile tier.', async () => {
+  //   const tiers = await omneo.profiles.calculateTiers(testProfileID)
+  //   Object.keys(mockTier).forEach((key) => {
+  //     expect(tiers).toHaveProperty(key)
+  //   })
 
-    Object.keys(mockTier.current_tier).forEach((key) => {
-      expect(tiers.current_tier).toHaveProperty(key)
-    })
-  })
+  //   Object.keys(mockTier.current_tier).forEach((key) => {
+  //     expect(tiers.current_tier).toHaveProperty(key)
+  //   })
+  // })
 })
