a7a49e9d0d56820a4fe3c9751440ae120f112420
Oliver Tribe <oliver@arkade.com.au>
2025-02-05 11:28:23 +1100

    test: add tests for calculate-tiers

--- src/tests/integration/omneo/profiles/tiers/calculate-tiers.test.ts
+++ src/tests/integration/omneo/profiles/tiers/calculate-tiers.test.ts
@@ -1,27 +1,23 @@
-// TODO Cannot calculate tiers on profiles without tier
-// Cannot delete a tier for testing
-// Tests can be added once omneo bugs are fixed
+import { describe, expect, test } from 'vitest'
+import { Omneo } from '../../../../..'
+import { tier as mockTier } from '../../../../mocks/tier/tier'
 
-import { describe, test } from 'vitest'
-// import { Omneo } from '../../../../..'
-// import { tier as mockTier } from '../../../../mocks/tier/tier'
+const omneo = new Omneo({
+  tenant: process.env.OMNEO_TENANT as string,
+  token: process.env.OMNEO_TOKEN as string
+})
 
-// const omneo = new Omneo({
-//   tenant: process.env.OMNEO_TENANT as string,
-//   token: process.env.OMNEO_TOKEN as string
-// })
-
-// const testProfileID = process.env.OMNEO_TEST_PROFILE_ID as string
+const testProfileID = process.env.OMNEO_TEST_PROFILE_ID as string
 
 describe('Calculate Profile Tiers', () => {
   test('SDK can calculate a profile tier.', async () => {
-    // const tiers = await omneo.profiles.calculateTiers(testProfileID)
-    // Object.keys(mockTier).forEach((key) => {
-    //   expect(tiers).toHaveProperty(key)
-    // })
+    const tiers = await omneo.profiles.tiers.calculate(testProfileID)
+    Object.keys(mockTier).forEach((key) => {
+      expect(tiers).toHaveProperty(key)
+    })
 
-    // Object.keys(mockTier.current_tier).forEach((key) => {
-    //   expect(tiers.current_tier).toHaveProperty(key)
-    // })
+    Object.keys(mockTier.current_tier).forEach((key) => {
+      expect(tiers.current_tier).toHaveProperty(key)
+    })
   })
 })
