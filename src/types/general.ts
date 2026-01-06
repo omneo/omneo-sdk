4f4e9f336e546c8bd450f2a151da4e96110a43f6
Johny Xu <johny.xu@arkade.com.au>
2026-01-07 10:46:02 +1100

    fix: update list custom fields api

--- src/types/general.ts
+++ src/types/general.ts
@@ -1,17 +1,25 @@
 export type CustomField = {
-    name: string
+    name?: string
     handle: string
     namespace: string
     value: string | { [key: string]: any} | any[] | number | boolean | null
     type: 'string'| 'integer'| 'float'| 'boolean'| 'json'| 'array'
-    created_at: string
-    updated_at: string
-    version: null | string
+    custom_fieldable_type?: 'profile' | 'list' | 'order' | 'item' | 'achievement_definition' | 'achievement_level'
+    custom_fieldable?: any
+    created_at?: string
+    updated_at?: string
+    version?: null | string |number
+    is_index?: boolean | null
 }
 
 export type CustomFieldInput = {
-    name: CustomField['name']
+    name?: CustomField['name']
+    type: CustomField['type']
     handle: CustomField['handle']
     namespace: CustomField['namespace']
     value: CustomField['value']
 }
+
+type PartialCustomFieldKeys = 'type' | 'handle' | 'namespace'
+export type UpdateCustomFieldInput = Omit<CustomFieldInput, PartialCustomFieldKeys> &
+  Partial<Pick<CustomFieldInput, PartialCustomFieldKeys>>
